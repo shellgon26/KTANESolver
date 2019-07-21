246aaef0d8795cec73c12782a0e3c2e6c250b9e6
shellgon26 <53082631+shellgon26@users.noreply.github.com>
2019-07-21 13:05:17 +0100

    Add files via upload

---
+++ speechrecog/someonessolution/someonessolution/Module1.vb
@@ -0,0 +1,42 @@
+﻿' NOTE: Must target .NET Framework, 3.0 or later (not .NET Core!)
+Imports System.Console
+
+' Make reference to System.Speech (System.Speech.dll)
+' https://docs.microsoft.com/en-us/dotnet/api/system.speech.recognition.speechrecognitionengine
+Imports System.Speech.Recognition
+
+Module Module1
+    Sub Main()
+
+        ' Create an in-process speech recognizer for the en-US locale.  
+        Using recognizer As New SpeechRecognitionEngine(
+          New Globalization.CultureInfo("en-US"))
+
+            ' Create and load a dictation grammar.  
+            recognizer.LoadGrammar(New DictationGrammar())
+
+            ' Add a handler for the speech recognized event.
+            AddHandler recognizer.SpeechRecognized,
+                AddressOf recognizer_SpeechRecognized
+
+            ' Configure input to the speech recognizer.  
+            recognizer.SetInputToDefaultAudioDevice()
+
+            ' Start asynchronous, continuous speech recognition.  
+            recognizer.RecognizeAsync(RecognizeMode.Multiple)
+
+            ' Keep the console window open.  
+            Do
+                ReadLine()
+            Loop
+
+        End Using
+
+    End Sub
+
+    ' Handle the SpeechRecognized event.  
+    Sub recognizer_SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+        WriteLine("Recognized text: " + e.Result.Text)
+    End Sub
+
+End Module
