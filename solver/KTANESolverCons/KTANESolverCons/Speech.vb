2ad7fd62c838e223a403452cded0bbac89cfa703
shellgon26 <53082631+shellgon26@users.noreply.github.com>
2019-09-25 12:34:14 +0100

    Update Speech.vb

--- solver/KTANESolverCons/KTANESolverCons/Speech.vb
+++ solver/KTANESolverCons/KTANESolverCons/Speech.vb
@@ -1,21 +1,33 @@
-﻿Module Speech
-    Public WithEvents recognizer As New System.Speech.Recognition.SpeechRecognitionEngine
-    Dim gram As New System.Speech.Recognition.DictationGrammar()
-    Dim cmd As String
-    Public Sub Load()
-        recognizer.SetInputToDefaultAudioDevice()
-        recognizer.RecognizeAsync()
+Imports System.Speech.Recognition
+Module Speech
+
+    Public Sub InitialiseSpeech()
+        ' Create an in-process speech recognizer for the en-US locale.  
+        Using recognizer As New SpeechRecognitionEngine(
+      New Globalization.CultureInfo("en-GB"))
+
+            ' Create and load a dictation grammar.  
+            recognizer.LoadGrammar(New DictationGrammar())
+
+            ' Add a handler for the speech recognized event.
+            AddHandler recognizer.SpeechRecognized,
+                AddressOf recognizer_SpeechRecognized
+
+            ' Configure input to the speech recognizer.  
+            recognizer.SetInputToDefaultAudioDevice()
+
+            ' Start asynchronous, continuous speech recognition.  
+            recognizer.RecognizeAsync(RecognizeMode.Multiple)
+
+            ' Keep the console window open.  
+        End Using
+
     End Sub
-    Private Sub GotSpeech(ByVal phrase As System.Speech.Recognition.SpeechRecognizedEventArgs)
-        cmd = phrase.Result.Text
-        If cmd.IndexOf("Run") <> 0 Or cmd.IndexOf("run") <> 0 Then
-            If cmd.Split(" ")(1) = "Notepad" Or cmd.Split(" ")(1) = "notepad" Then
 
-                voice.Speak("Running Notepad.")
-
-                Shell("notepad.exe", AppWinStyle.NormalFocus, False)
-
-            End If
-        End If
+    ' Handle the SpeechRecognized event.  
+    Sub recognizer_SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+        speechdetect = True
+        mostrecentspeech = e.Result.Text
     End Sub
+
 End Module
